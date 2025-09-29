93d09451aca903a6ad9e7bcf7c4a7770e71ecc60
anton <voidnamespace@outlook.com>
2025-09-29 11:50:02 +0300

    going to superleaern middleware, now going on puase on this branch and create other

---
+++ InventoryShop.Api/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿namespace IS.Controller;
+
+using IS.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly ProductService _productService;
+
+    public ProductController (ProductService productService)
+    {
+        _productService = productService;
+    }
+
+
+
+
+
+
+
+
+}
+
---
+++ InventoryShop.Api/Services/ProductService.cs
@@ -0,0 +1,31 @@
+﻿namespace IS.Services;
+using IS.DbContext;
+using IS.Entities;
+using Microsoft.EntityFrameworkCore;
+
+public class ProductService
+{
+    private readonly ILogger<ProductService> _logger;
+    private readonly AppDbContext _context;
+
+    public ProductService(ILogger<ProductService> logger, AppDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+
+    }
+
+
+    public async Task<List<Product> GetFullListProducts()
+    {
+        var list = await _context.Products.ToListAsync();
+           
+    }
+
+
+
+
+
+
+}
+
